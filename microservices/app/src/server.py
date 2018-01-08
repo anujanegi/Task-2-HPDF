4a50e78b9cc95fec636bdb6ed02063a31281c1bf
anujanegi <anujanegi1@gmail.com>
2018-01-09 00:12:09 +0530

    Task 1

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,13 +1,6 @@
 from src import app
-# from flask import jsonify
 
 
 @app.route("/")
-def home():
-    return "Hasura Hello World"
-
-# Uncomment to add a new URL at /new
-
-# @app.route("/json")
-# def json_message():
-#     return jsonify(message="Hello World")
+def index():
+    return "Hello World -Anuja Negi"
