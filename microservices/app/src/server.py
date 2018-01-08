982b0a64654867ff8fc88032891241b4b1f0ecab
anujanegi <anujanegi1@gmail.com>
2018-01-09 00:16:50 +0530

    Task 2

--- microservices/app/src/server.py
+++ microservices/app/src/server.py
@@ -1,6 +1,35 @@
 from src import app
+import requests
+import json
 
-
-@app.route("/")
+#
+# Task 1
+#
+@app.route('/')
 def index():
-    return "Hello World -Anuja Negi"
+    return 'Hello World -Anuja Negi'
+
+#
+# Task 2
+#
+@app.route('/authors')
+def authors():
+    url1 = "https://jsonplaceholder.typicode.com/users"
+    url2 = "https://jsonplaceholder.typicode.com/posts"
+    try:
+        author = requests.get(url1)
+        count = requests.get(url2)
+    except requests.ConnectionError:
+        return "Connection Error"
+
+    authorNames = json.loads(author.text)
+    postCount = json.loads(count.text)
+
+    text=""
+    for i in range(len(authorNames)):
+        c=0
+        for j in range(len(postCount)):
+            if postCount[j]['userId']==authorNames[i]['id']:
+                c+=1
+        text=text+authorNames[i]['name']+" "+str(c)+"<br>"
+    return text
